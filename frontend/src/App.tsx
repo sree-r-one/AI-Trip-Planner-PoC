30a91997130f6eb5912260cceda99d66cbd412b3
sree <sreeraj_ec@hotmail.com>
2025-02-04 13:02:17 +0800

    feat(setup): Setup Completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div className="flex bg-red-200 p-3">App</div>;
+  return (
+    <div className="flex bg-red-200 p-3">
+      App
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
